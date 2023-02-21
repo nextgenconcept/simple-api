5952a886df54d56d2ee302157a836fd05df875ef
citizen63000 <citizen.lantien@gmail.com>
2023-02-21 18:57:56 +0100

    create user command

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User\User;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class CreateUserCommand extends Command
+{
+    protected static $defaultName = 'app:create-user';
+    protected static $defaultDescription = 'Creates a new user.';
+
+    protected ?ManagerRegistry $managerRegistry;
+    protected ?UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(string $name = null, ManagerRegistry $managerRegistry = null, UserPasswordHasherInterface $passwordHasher = null)
+    {
+        parent::__construct($name);
+        $this->managerRegistry = $managerRegistry;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            // ...
+            ->addArgument('email', InputArgument::REQUIRED, 'User email')
+            ->addArgument('firstname', InputArgument::REQUIRED, 'User firstname')
+            ->addArgument('lastname', InputArgument::REQUIRED, 'User lastname')
+            ->addArgument('password', InputArgument::REQUIRED, 'User password')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = new User();
+        $user->setEmail($input->getArgument('email'));
+        $user->setUsername($input->getArgument('email'));
+        $user->setFirstname($input->getArgument('firstname'));
+        $user->setLastname($input->getArgument('lastname'));
+        $user->setPassword($this->passwordHasher->hashPassword($user, $input->getArgument('password')));
+
+        $this->managerRegistry->getManager()->persist($user);
+        $this->managerRegistry->getManager()->flush();
+
+        return Command::SUCCESS;
+    }
+}
--- src/Entity/User/User.php
+++ src/Entity/User/User.php
@@ -12,4 +12,46 @@ use EasyApiJwtAuthentication\Entity\AbstractExtendedUser;
  */
 class User extends AbstractExtendedUser
 {
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected ?string $firstname = null;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected ?string $lastname = null;
+
+    /**
+     * @return string|null
+     */
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string|null $firstname
+     */
+    public function setFirstname(?string $firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string|null $lastname
+     */
+    public function setLastname(?string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
 }
