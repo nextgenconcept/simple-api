b423c5e1edbdd33dded57f466b051fcd75aca1c8
citizen63000 <citizen.lantien@gmail.com>
2023-02-21 20:31:26 +0100

    book entity

---
+++ src/Entity/Book.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use EasyApiBundle\Entity\AbstractBaseEntity;
+
+/**
+ * @ORM\Entity()
+ */
+class Book extends AbstractBaseEntity
+{
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected ?string $title = null;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected ?string $author = null;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected ?string $summary = null;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected ?int $year = null;
+
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string|null $title
+     */
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string|null $author
+     */
+    public function setAuthor(?string $author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSummary(): ?string
+    {
+        return $this->summary;
+    }
+
+    /**
+     * @param string|null $summary
+     */
+    public function setSummary(?string $summary): void
+    {
+        $this->summary = $summary;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param int|null $year
+     */
+    public function setYear(?int $year): void
+    {
+        $this->year = $year;
+    }
+
+}
