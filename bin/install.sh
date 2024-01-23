64c23a09fe20d7711ef440ce885d1b099d0044a9
lionel <contact.ivolc@gmail.com>
2024-01-23 09:41:35 +0100

    some fixes

--- bin/install.sh
+++ bin/install.sh
@@ -3,15 +3,6 @@
 # script à lancer depuis la racine du projet
 
 STEP=1
-#
-#echo "${STEP} - Set app/config/parameters_$1.yml.dist"
-#sed -i "s/%JWT-PASSPHRASE%/$2/g" app/config/parameters_$1.yml.dist
-#cp app/config/parameters_$1.yml.dist app/config/parameters.yml.dist
-#((STEP++))
-#echo "Done."
-#
-#
-
 echo "${STEP} - Set rights on framework files"
 chown -R www-data var/*
 chmod -R 777 *
@@ -19,7 +10,7 @@ HTTPDUSER=$(ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]gi
 setfacl -dR -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX var
 setfacl -R -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX var
 ((STEP++))
-#echo "Done."
+echo "Done."
 
 echo "${STEP} - Install vendors and clear cache"
 composer install --no-interaction --profile -vvv
